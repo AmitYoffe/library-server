3c8621df7f2781c4e97471b3cd9d5f6a94c45df2
Amit Yoffe <yoffe.amit@gmail.com>
2024-11-12 11:34:34 +0200

    - Bug fix

--- src/borrows/borrows.module.ts
+++ src/borrows/borrows.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book, User } from 'src';
-import { Borrow, BorrowsService} from './index';
+import { Borrow } from './borrow.entity';
+import { BorrowsService } from './borrows.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Borrow, Book, User])],
