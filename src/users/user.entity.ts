36f6c2a79b260cf14afcbd0e6031432bc498d36f
persil <alono12072005@gmail.com>
2024-12-04 15:53:08 +0200

    added unique field

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -2,28 +2,28 @@ import { BorrowEntity } from 'src/borrows';
 import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from 'typeorm';
 
 @Entity('users')
+@Unique(["username"])
 export class UserEntity extends BaseEntity {
   @PrimaryGeneratedColumn({
     type: 'integer',
   })
   id: number;
 
-  // @Unique()
   @Column({
     type: 'text',
   })
   username: string;
-
+  
   @Column({
     type: 'boolean',
   })
   isAdmin: boolean;
-
+  
   @Column({
     type: 'text',
   })
   password: string;
-
+  
   @OneToMany(() => BorrowEntity, (borrow) => borrow.user)
   borrows: BorrowEntity[];
 }
